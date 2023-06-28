b96d3d1d734c89d65694e7d690ea97779db15cc8
Christabel Aidoo <129256391+christabely@users.noreply.github.com>
2023-06-28 06:50:00 +0100

    Update chro11.c

--- chro11.c
+++ chro11.c
@@ -6,26 +6,25 @@
  */
 void cj_rev_string(char *s)
 {
-    int ct = 0, y, z;
-    char *str, tmp;
+int ct = 0, y, z;
+char *str, tmp;
 
-    for (ct = 0; s[ct] != '\0'; ct++)
-    {
-        str = s;
-    }
-
-    y = 0;
-    while (y < ct - 1)
-    {
-        z = y + 1;
-        while (z > 0)
-        {
-            tmp = *(str + z);
-            *(str + z) = *(str + (z - 1));
-            *(str + (z - 1)) = tmp;
-            z--;
-        }
-        y++;
-    }
+for (ct = 0; s[ct] != '\0'; ct++)
+{
+str = s;
 }
 
+y = 0;
+while (y < ct - 1)
+{
+z = y + 1;
+while (z > 0)
+{
+tmp = *(str + z);
+*(str + z) = *(str + (z - 1));
+*(str + (z - 1)) = tmp;
+z--;
+}
+y++;
+}
+}
