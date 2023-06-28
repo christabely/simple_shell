01cf2c648329763300c83c21dcd3592bd4bcce9e
Christabel Aidoo <129256391+christabely@users.noreply.github.com>
2023-06-28 04:36:16 +0100

    Update chro22.c

--- chro22.c
+++ chro22.c
@@ -2,32 +2,32 @@
 
 /**
  * cj_get_help - function that retrieves help messages according builtin
- * @dsh: data structure (args and input)
+ * @dtsh: data structure (args and input)
  * Return: Return 0
 */
-int cj_get_help(proshell_shell *dsh)
+int cj_get_help(proshell_shell *dtsh)
 {
 
-	if (dsh->args[1] == 0)
+	if (dtsh->args[1] == 0)
 		ax_help_general();
-	else if (cj_strcmp(dsh->args[1], "setenv") == 0)
+	else if (cj_strcmp(dtsh->args[1], "setenv") == 0)
 		ax_help_setenv();
-	else if (cj_strcmp(dsh->args[1], "env") == 0)
+	else if (cj_strcmp(dtsh->args[1], "env") == 0)
 		ax_help_env();
-	else if (cj_strcmp(dsh->args[1], "unsetenv") == 0)
+	else if (cj_strcmp(dtsh->args[1], "unsetenv") == 0)
 		ax_help_unsetenv();
-	else if (cj_strcmp(dsh->args[1], "help") == 0)
+	else if (cj_strcmp(dtsh->args[1], "help") == 0)
 		ax_help();
-	else if (cj_strcmp(dsh->args[1], "exit") == 0)
+	else if (cj_strcmp(dtsh->args[1], "exit") == 0)
 		ax_help_exit();
-	else if (cj_strcmp(dsh->args[1], "cd") == 0)
+	else if (cj_strcmp(dtsh->args[1], "cd") == 0)
 		ax_help_cd();
-	else if (cj_strcmp(dsh->args[1], "alias") == 0)
+	else if (cj_strcmp(dtsh->args[1], "alias") == 0)
 		ax_help_alias();
 	else
-		write(STDERR_FILENO, dsh->args[0],
-		      cj_strlen(dsh->args[0]));
+		write(STDERR_FILENO, dtsh->args[0],
+		      cj_strlen(dtsh->args[0]));
 
-	dsh->status = 0;
+	dtsh->status = 0;
 	return (1);
 }
