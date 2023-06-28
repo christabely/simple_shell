5171718d24b0aa4c7c825c3eb88cb5d266017393
Christabel Aidoo <129256391+christabely@users.noreply.github.com>
2023-06-28 03:25:47 +0100

    Update chro12.c

--- chro12.c
+++ chro12.c
@@ -1,157 +1,156 @@
-#include "phiros.h"
+#include "proshell.h"
 /**
- * pr_cd_dot - changes to the parent directory
- * @dsh: data relevant (environ)
+ * cj_cd_dot - changes to the parent directory
+ * @dtsh: data relevant (environ)
  * Return: no return
  */
-void pr_cd_dot(phiros_shell *dsh)
+void cj_cd_dot(project_shell *dtsh)
 {
 	char pwd[PATH_MAX];
 	char *d, *cp_pwd, *cp_strtok_pwd;
 
 	getcwd(pwd, sizeof(pwd));
-	cp_pwd = pr_strdup(pwd);
-	pr_set_env("OLDPWD", cp_pwd, dsh);
-	d = dsh->args[1];
-	if (pr_strcmp(".", d) == 0)
+	cp_pwd = cj_strdup(pwd);
+	cj_set_env("OLDPWD", cp_pwd, dtsh);
+	d = dtsh->args[1];
+	if (cj_strcmp(".", d) == 0)
 	{
-		pr_set_env("PWD", cp_pwd, dsh);
+		cj_set_env("PWD", cp_pwd, dtsh);
 		free(cp_pwd);
 		return;
 	}
-	if (pr_strcmp("/", cp_pwd) == 0)
+	if (cj_strcmp("/", cp_pwd) == 0)
 	{
 		free(cp_pwd);
 		return;
 	}
 	cp_strtok_pwd = cp_pwd;
-	pr_rev_string(cp_strtok_pwd);
-	cp_strtok_pwd = pr_strtok(cp_strtok_pwd, "/");
+	cj_rev_string(cp_strtok_pwd);
+	cp_strtok_pwd = cj_strtok(cp_strtok_pwd, "/");
 	if (cp_strtok_pwd != NULL)
 	{
-		cp_strtok_pwd = pr_strtok(NULL, "\0");
+		cp_strtok_pwd = cj_strtok(NULL, "\0");
 
 		if (cp_strtok_pwd != NULL)
-			pr_rev_string(cp_strtok_pwd);
+			cj_rev_string(cp_strtok_pwd);
 	}
 	if (cp_strtok_pwd != NULL)
 	{
 		chdir(cp_strtok_pwd);
-		pr_set_env("PWD", cp_strtok_pwd, dsh);
+		cj_set_env("PWD", cp_strtok_pwd, dtsh);
 	}
 	else
 	{
 		chdir("/");
-		pr_set_env("PWD", "/", dsh);
+		cj_set_env("PWD", "/", dtsh);
 	}
-	dsh->status = 0;
+	dtsh->status = 0;
 	free(cp_pwd);
 }
-
 /**
- * pr_cd_to - changes to a dir given by user
- * @dsh: data relevant (directories)
+ * cj_cd_to - changes to a dir given by user
+ * @dtsh: data relevant (directories)
  * Return: no return
  */
-void pr_cd_to(phiros_shell *dsh)
+void cj_cd_to(project_shell *dtsh)
 {
 	char pwd[PATH_MAX];
 	char *d, *cp_pwd, *cp_dir;
 
 	getcwd(pwd, sizeof(pwd));
 
-	d = dsh->args[1];
+	d = dtsh->args[1];
 	if (chdir(d) == -1)
 	{
-		pr_get_error(dsh, 2);
+		cj_get_error(dtsh, 2);
 		return;
 	}
 
-	cp_pwd = pr_strdup(pwd);
-	pr_set_env("OLDPWD", cp_pwd, dsh);
+	cp_pwd = cj_strdup(pwd);
+	cj_set_env("OLDPWD", cp_pwd, dtsh);
 
-	cp_dir = pr_strdup(d);
-	pr_set_env("PWD", cp_dir, dsh);
+	cp_dir = cj_strdup(d);
+	cj_set_env("PWD", cp_dir, dtsh);
 
 	free(cp_pwd);
 	free(cp_dir);
 
-	dsh->status = 0;
+	dtsh->status = 0;
 
 	chdir(d);
 }
+
 /**
- * pr_cd_previous - changes to the previous directory
- * @dsh: data relevant (environ)
+ * cj_cd_previous - changes to the previous directory
+ * @dtsh: data relevant (environ)
  * Return: no return
  */
-void pr_cd_previous(phiros_shell *dsh)
+void cj_cd_previous(project_shell *dtsh)
 {
 	char pwd[PATH_MAX];
 	char *p_pwd, *p_oldpwd, *cp_pwd, *cp_oldpwd;
 
 	getcwd(pwd, sizeof(pwd));
-	cp_pwd = pr_strdup(pwd);
+	cp_pwd = cj_strdup(pwd);
 
-	p_oldpwd = pr_getenv("OLDPWD", dsh->_environ);
+	p_oldpwd = cj_getenv("OLDPWD", dtsh->_environ);
 
 	if (p_oldpwd == NULL)
 		cp_oldpwd = cp_pwd;
 	else
-		cp_oldpwd = pr_strdup(p_oldpwd);
+		cp_oldpwd = cj_strdup(p_oldpwd);
 
-	pr_set_env("OLDPWD", cp_pwd, dsh);
+	cj_set_env("OLDPWD", cp_pwd, dtsh);
 
 	if (chdir(cp_oldpwd) == -1)
-		pr_set_env("PWD", cp_pwd, dsh);
+		cj_set_env("PWD", cp_pwd, dtsh);
 	else
-		pr_set_env("PWD", cp_oldpwd, dsh);
+		cj_set_env("PWD", cp_oldpwd, dtsh);
 
-	p_pwd = pr_getenv("PWD", dsh->_environ);
+	p_pwd = cj_getenv("PWD", dtsh->_environ);
 
-	write(STDOUT_FILENO, p_pwd, pr_strlen(p_pwd));
+	write(STDOUT_FILENO, p_pwd, cj_strlen(p_pwd));
 	write(STDOUT_FILENO, "\n", 1);
 
 	free(cp_pwd);
 	if (p_oldpwd)
 		free(cp_oldpwd);
 
-	dsh->status = 0;
+	dtsh->status = 0;
 
 	chdir(p_pwd);
 }
-
 /**
- * pr_cd_to_home - changes to home directory
- * @dsh: data relevant (environ)
+ * cj_cd_to_home - changes to home directory
+ * @dtsh: data relevant (environ)
  * Return: no return
  */
-void pr_cd_to_home(phiros_shell *dsh)
+void cj_cd_to_home(project_shell *dtsh)
 {
-	char *p_pwd, *home;
-	char pwd[PATH_MAX];
+    char *p_pwd, *home;
+    char pwd[PATH_MAX];
 
-	getcwd(pwd, sizeof(pwd));
-	p_pwd = pr_strdup(pwd);
+    getcwd(pwd, sizeof(pwd));
+    p_pwd = cj_strdup(pwd);
 
-	home = pr_getenv("HOME", dsh->_environ);
+    home = cj_getenv("HOME", dtsh->_environ);
 
-	if (home == NULL)
-	{
-		pr_set_env("OLDPWD", p_pwd, dsh);
-		free(p_pwd);
-		return;
-	}
+    if (home == NULL)
+    {
+        cj_set_env("OLDPWD", p_pwd, dtsh);
+        free(p_pwd);
+        return;
+    }
 
-	if (chdir(home) == -1)
-	{
-		pr_get_error(dsh, 2);
-		free(p_pwd);
-		return;
-	}
+    if (chdir(home) == -1)
+    {
+        cj_get_error(dtsh, 2);
+        free(p_pwd);
+        return;
+    }
 
-	pr_set_env("OLDPWD", p_pwd, dsh);
-	pr_set_env("PWD", home, dsh);
-	free(p_pwd);
-	dsh->status = 0;
+    cj_set_env("OLDPWD", p_pwd, dtsh);
+    cj_set_env("PWD", home, dtsh);
+    free(p_pwd);
+    dtsh->status = 0;
 }
